72a170df277cfb924ca1ce25a2ba41d455aab1d7
unknown <luisfelipesdn12@gmail.com>
2020-12-07 11:25:18 -0300

    Added errors test cases

--- test.js
+++ test.js
@@ -1,6 +1,25 @@
+const os = require("os")
 const test = require("ava")
 const minecraftPath = require(".")
 
 test("main", t => {
 	t.is(typeof minecraftPath(), "string")
+})
+
+test("platform not supported error", t => {
+	const supported = ["win32", "darwin", "linux", "android"]
+
+	if (supported.includes(process.platform)) {
+		t.notThrows(() => minecraftPath(), `${process.platform} is not supported!`)
+	} else {
+		t.throws(() => minecraftPath(), `${process.platform} is not supported!`)
+	}
+})
+
+test("Windows 10 is required error", t => {
+	if (process.platform !== "win32" || Number.parseInt(os.release().split(".")[0], 10) !== 10) {
+		t.throws(() => minecraftPath.win10(), "Windows 10 is required!")
+	} else {
+		t.notThrows(() => minecraftPath.win10(), "Windows 10 is required!")
+	}
 })
